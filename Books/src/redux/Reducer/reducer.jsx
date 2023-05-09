e7ecb658d7a422a3215a4d654a9240c392526adc
Gaston Garcia Juri <gastongarciajuri@gmail.com>
2023-05-09 19:47:16 -0300

    redux

---
+++ Books/src/redux/Reducer/reducer.jsx
@@ -0,0 +1,14 @@
+const initialState = {
+    counter: 0
+  };
+  
+  function rootReducer(state = initialState, action) {
+    switch (action.type) {
+
+      default:
+        return state;
+    }
+  }
+  
+  export default rootReducer;
+  
